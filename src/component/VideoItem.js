efafdedca73fcdac23210974acb1cbae200c8f55
mcurtis91 <mikecurtis1991@gmail.com>
2019-08-08 13:00:34 -0700

    Youtube Videos

---
+++ src/component/VideoItem.js
@@ -0,0 +1,22 @@
+import "./VideoItem.css";
+import React from "react";
+
+const VideoItem = ({ video, onVideoSelect }) => {
+  return (
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt=""
+      />
+      <div className="content">
+        <div
+          className="header"
+          dangerouslySetInnerHTML={{ __html: video.snippet.title }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default VideoItem;
